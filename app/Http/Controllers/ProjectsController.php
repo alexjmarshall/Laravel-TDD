be2794b69c5d3365cd8c1d0fa89b4977e5819663
alexjmarshall <marshalexander@gmail.com>
2019-03-11 12:54:11 -0600

    finish episode 5

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index')->with('projects', $projects);
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
+        
+        Project::create($attributes);
+
+        return redirect('/projects');
+    }
+
+    public function show(Project $project)
+    {
+        return view('projects.show')->with('project', $project);
+    }
+}
